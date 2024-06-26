0cbffdb211a8509a27f520e4bd33963861342d58
Mujeeb218353 <139048778+Mujeeb218353@users.noreply.github.com>
2024-06-27 02:28:59 +0500

    fixed some issues

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,6 +23,9 @@ function Navbar  ()  {
                             <li className="nav-item">
                                 <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} aria-current="page" to="/">Home</Link>
                             </li>
+                            <li className="nav-item">
+                                <Link className={`nav-link ${location.pathname === "/about" ? "active" : ""}`} to="/about">About</Link>
+                            </li>
                         </ul>
                         {
                             !localStorage.getItem('token') ?
